ae0457c735e5786ed9c6125ed08a56d40d60529b
ThawThuHan <thawthuhan9@gmail.com>
2025-06-20 14:22:40 +0700

    [added:doc]API Documentation for GLAcc, Project and PaymentMethod Controller

--- controllers/GLAccController.go
+++ controllers/GLAccController.go
@@ -16,6 +16,16 @@ func NewGLAccController(glAccService *services.GLAccService) *GLAccController {
 	return &GLAccController{GLAccService: glAccService}
 }
 
+// SyncGLAcc synchonizes GLAcc data from SQLAcc API
+// @Summary Sync GLAcc
+// @Description Sync GLAcc data from SQLAcc API
+// @Tags GLAcc
+// @Accept json
+// @Produce json
+// @Success 200 {object} map[string]interface{}
+// @Failure 500 {object} map[string]interface{}
+// @Router /gl-acc/sync [post]
+// @Security JWT Token
 func (c *GLAccController) SyncGLAcc(ctx echo.Context) error {
 	if err := c.GLAccService.SyncGLAcc(); err != nil {
 		log.Printf("Error syncing GLAcc: %v", err)
@@ -24,6 +34,16 @@ func (c *GLAccController) SyncGLAcc(ctx echo.Context) error {
 	return ctx.JSON(http.StatusOK, map[string]interface{}{"message": "GLAcc synced successfully"})
 }
 
+// GetGLAcc fetches GLAcc data
+// @Summary Get GLAcc
+// @Description Get GLAcc data
+// @Tags GLAcc
+// @Accept json
+// @Produce json
+// @Success 200 {object} []models.GLAcc
+// @Failure 500 {object} map[string]interface{}
+// @Router /gl-acc [get]
+// @Security JWT Token
 func (c *GLAccController) GetGLAcc(ctx echo.Context) error {
 	glAcc, err := c.GLAccService.GetGLAcc()
 	if err != nil {
--- controllers/PaymentMethodController.go
+++ controllers/PaymentMethodController.go
@@ -16,6 +16,16 @@ func NewPaymentMethodController(paymentMethodService *services.PaymentMethodServ
 	return &PaymentMethodController{paymentMethodService: paymentMethodService}
 }
 
+// SyncPaymentMethods synchronizes payment methods from the SQLACC API
+// @Summary Sync payment methods
+// @Description Synchronizes payment methods from the SQLACC API
+// @Tags Payment Method
+// @Accept json
+// @Produce json
+// @Success 200 {object} string
+// @Failure 500 {object} string
+// @Router /payment-methods/sync [post]
+// @Security JWT Token
 func (c *PaymentMethodController) SyncPaymentMethods(ctx echo.Context) error {
 	err := c.paymentMethodService.SyncPaymentMethods()
 	if err != nil {
@@ -25,6 +35,16 @@ func (c *PaymentMethodController) SyncPaymentMethods(ctx echo.Context) error {
 	return ctx.JSON(http.StatusOK, "Payment methods synced successfully")
 }
 
+// GetPaymentMethods fetches payment methods
+// @Summary Get payment methods
+// @Description Fetches payment methods
+// @Tags Payment Method
+// @Accept json
+// @Produce json
+// @Success 200 {object} []models.PaymentMethod
+// @Failure 404 {object} string
+// @Router /payment-methods [get]
+// @Security JWT Token
 func (c *PaymentMethodController) GetPaymentMethods(ctx echo.Context) error {
 	paymentMethods, err := c.paymentMethodService.GetPaymentMethods()
 	if err != nil {
